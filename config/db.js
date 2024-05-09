d3a5497ee1d506ef04799c6c0adee0c315ae8e09
Visal20497 <visal20497@gmail.com>
2024-05-10 04:55:10 +0530

    deployment

--- config/db.js
+++ config/db.js
@@ -3,12 +3,8 @@ import mongoose from "mongoose";
 // MongoDb Database Connection
 let DbConnection=async()=>{
     try{
-<<<<<<< HEAD
         let connection=await mongoose.connect(process.env.MONGOOSEDB_URL)
        // mongodb://127.0.0.1:27017/MYEcom
-=======
-        let connection=await mongoose.connect('mongodb+srv://visal20497:wpxFBHtGfSSZEhZR@cluster0.zxg4jay.mongodb.net/MYEcom?retryWrites=true&w=majority&appName=Cluster0')
->>>>>>> 5b3f07ebdc2fc79783e40b15496c1d78e25875f1
         console.log("Mongodb connnected!")
     }
     catch(error){
